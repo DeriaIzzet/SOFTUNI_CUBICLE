9c0c51f003cdda2eddde6d78098ff15df9b140e5
DeriaIzzet <deriaizzzet@gmail.com>
2023-09-26 15:41:43 +0300

    Add Express and Handlebars to project
    Extract common HTML to layout

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const handlebars = require("express-handlebars")
+const path = require ('path')
+
+const app = express()
+
+const PORT = 5000
+
+
+app.engine('hbs', handlebars.engine ({
+    extname: "hbs"
+}))
+app.set('view engine','hbs')
+app.set('views', 'src/views')
+
+app.get('/',(req,res)=>{
+
+res.render('index')
+
+})
+
+app.listen(PORT ,()=> console.log(`server is listening on port ${PORT}...`))
