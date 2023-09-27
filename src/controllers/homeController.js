ae138576e74ee8186a72c7307420a8838378897d
DeriaIzzet <deriaizzzet@gmail.com>
2023-09-27 23:58:31 +0300

    Add search bar

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,8 +1,11 @@
-const  router = require('express').Router()
+const router = require('express').Router()
 const cubeManager = require('../menagers/cubeManager')
 router.get('/', (req, res) => {
-    const cubes = cubeManager.getAll()
-    res.render('index',{cubes})
+    const { search, from, to } = req.query;
+
+    const cubes = cubeManager.getAll(search, from, to)
+    
+    res.render('index', { cubes })
 })
 router.get('/about', (req, res) => {
     res.render('about')
--- src/menagers/cubeManager.js
+++ src/menagers/cubeManager.js
@@ -1,15 +1,28 @@
 const uniqid = require('uniqid')
 const cubes = []
- 
-exports.getAll = ()=> cubes.slice()
-exports.getOne = (cubeId)=> cubes.find (x => x.id == cubeId)
+
+exports.getAll = (search, from, to) => {
+    let result = cubes.slice()
+
+    if (search) {
+        result = result.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()))
+    }
+    if (from) {
+        result = result.filter(cube => cube.difficultyLevel >= (Number(from)))
+    }
+    if (to) {
+        result = result.filter(cube => cube.difficultyLevel <= (Number(to)))
+    }
+    return result
+}
+exports.getOne = (cubeId) => cubes.find(x => x.id == cubeId)
 
 exports.create = (cubeData) => {
 
-    const newCube ={
+    const newCube = {
         id: uniqid(),
         ...cubeData,
     }
     cubes.push(newCube)
-    return  newCube
+    return newCube
 }
