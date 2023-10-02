1f74b89353d2b577d182605f58ac941f1da99ac3
DeriaIzzet <deriaizzzet@gmail.com>
2023-10-02 16:06:48 +0300

    Add post action for create accessory

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -3,5 +3,13 @@ const router = require('express').Router()
 router.get('/create', (req,res) => {
     res.render('accessory/create')
 })
+ router.post('/create',(req,res) =>{
+    const body = req.body
+       // const { name, description, imageUrl} = req.body
+      //  cubeManager.create({ name, description, imageUrl})
+      console.log(body);
+       res.redirect('/')
+    
+ })
 
 module.exports = router
