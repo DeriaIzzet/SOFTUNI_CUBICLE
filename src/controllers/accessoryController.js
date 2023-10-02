fa4887fde50021edb013a4031f934a2c43a02c28
DeriaIzzet <deriaizzzet@gmail.com>
2023-10-02 17:06:15 +0300

    add accessory to DB

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -1,15 +1,18 @@
 const router = require('express').Router()
+const accessoryManager = require('../menagers/accessoryManager')
 
-router.get('/create', (req,res) => {
+router.get('/create', (req, res) => {
     res.render('accessory/create')
 })
- router.post('/create',(req,res) =>{
+router.post('/create', async (req, res) => {
     const body = req.body
-       // const { name, description, imageUrl} = req.body
-      //  cubeManager.create({ name, description, imageUrl})
-      console.log(body);
-       res.redirect('/')
-    
- })
 
-module.exports = router
+    const { name, description, imageUrl } = req.body
+    await accessoryManager.create({ name, description, imageUrl })
+
+    console.log(body);
+    res.redirect('/')
+
+})
+
+module.exports = router 
