9f475aea5515a8044b26be437967f10eb91ad95e
DeriaIzzet <deriaizzzet@gmail.com>
2023-10-02 19:55:09 +0300

    Show accessories cube and accessories on attach page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,25 +1,30 @@
 const router = require('express').Router()
 const cubeManager = require('../menagers/cubeManager')
+const accessoryManager = require('../menagers/accessoryManager')
 
 router.get('/create', (req, res) => {
     res.render('create')
 })
 
-router.post('/create',async (req, res) => {
+router.post('/create', async (req, res) => {
     const { name, description, imageUrl, difficultyLevel } = req.body
-   await cubeManager.create({ name, description, imageUrl,difficultyLevel: Number(difficultyLevel)})
-   res.redirect('/')
+    await cubeManager.create({ name, description, imageUrl, difficultyLevel: Number(difficultyLevel) })
+    res.redirect('/')
 })
 
-router.get('/:cubeId/details', async(req, res) => {
+router.get('/:cubeId/details', async (req, res) => {
     const cube = await cubeManager.getOne(req.params.cubeId).lean()
-    if (!cube){
+
+    if (!cube) {
         return res.redirect('/404')
     }
     res.render('details', { cube })
 })
 
-router.get('/:cubeId/attach-accessory', async(req, res) => {
-    res.render('accessory/attach')
+router.get('/:cubeId/attach-accessory', async (req, res) => {
+    const cube = await cubeManager.getOne(req.params.cubeId).lean()
+    const accessories = await accessoryManager.getAll().lean()
+
+    res.render('accessory/attach', { cube, accessories })
 })
 module.exports = router   
