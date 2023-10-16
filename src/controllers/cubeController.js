41af3cb3d86a87778b0e8a9c6376ae177f9baa14
DeriaIzzet <deriaizzzet@gmail.com>
2023-10-16 16:47:59 +0300

    Add delete cube page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -4,13 +4,13 @@ const accessoryManager = require('../menagers/accessoryManager')
 
 router.get('/create', (req, res) => {
     console.log(req.user)
-    res.render('create')
+    res.render('cube/create')
 })
 
 router.post('/create', async (req, res) => {
 
     const { name, description, imageUrl, difficultyLevel } = req.body
-    await cubeManager.create({ name, description, imageUrl, difficultyLevel: Number(difficultyLevel),owner: req.user._id })
+    await cubeManager.create({ name, description, imageUrl, difficultyLevel: Number(difficultyLevel), owner: req.user._id })
     res.redirect('/')
 })
 
@@ -21,7 +21,7 @@ router.get('/:cubeId/details', async (req, res) => {
     if (!cube) {
         return res.redirect('/404')
     }
-    res.render('details', { cube })
+    res.render('cube/details', { cube })
 })
 
 router.get('/:cubeId/attach-accessory', async (req, res) => {
@@ -39,4 +39,10 @@ router.post('/:cubeId/attach-accessory', async (req, res) => {
 
     res.redirect(`/cubes/${cubeId}/details`)
 })
+
+router.get('/:cubeId/delete', async (req, res) => {
+    const cube = await cubeManager.getOne(req.params.cubeId).lean()
+    res.render('cube/delete', { cube })
+})
+
 module.exports = router   
